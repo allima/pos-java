fc5f5f7aee15095c614cd2710755ae9389432c9b
Alexandre de Lima <allima1991@gmail.com>
2026-09-10 15:22:10 -0300

    feat: update application properties for production and add MySQL dependency

--- 4_Desenvolvimento Back-End em Java com Spring Boot /minhas-tarefas/src/main/java/br/com/ropalon/tarefas/controller/TarefaController.java
+++ 4_Desenvolvimento Back-End em Java com Spring Boot /minhas-tarefas/src/main/java/br/com/ropalon/tarefas/controller/TarefaController.java
@@ -1,67 +1,67 @@
 package br.com.ropalon.tarefas.controller;
 
-import br.com.ropalon.tarefas.model.Tarefa;
-import br.com.ropalon.tarefas.repository.TarefaRepository;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ropalon.tarefas.model.Tarefa;
+import br.com.ropalon.tarefas.repository.TarefaRepository;
 
-import java.util.List;
-import java.util.Map;
-
+@RequestMapping("/tarefa")
 @RestController
 public class TarefaController {
 
-    private final TarefaRepository tarefaRepository;
+	private final TarefaRepository tarefaRepository;
 
-    public TarefaController(TarefaRepository tarefaRepository) {
-        this.tarefaRepository = tarefaRepository;
-    }
+	public TarefaController(TarefaRepository tarefaRepository) {
+		this.tarefaRepository = tarefaRepository;
+	}
 
-    @GetMapping("/tarefa")
-    public List<Tarefa> todasTarefas(@RequestParam Map<String, String> parametros) {
-        if (parametros.isEmpty()) {
-            return tarefaRepository.findAll();
-        }
-        var descricao = parametros.get("descricao");
-        return tarefaRepository.findByDescricaoLikeIgnoreCase("%"+descricao+"%");
-    }
+	@GetMapping
+	public List<Tarefa> todasTarefas(@RequestParam Map<String, String> parametros) {
+		if (parametros.isEmpty()) {
+			return tarefaRepository.findAll();
+		}
+		var descricao = parametros.get("descricao");
+		return tarefaRepository.findByDescricaoLikeIgnoreCase("%" + descricao + "%");
+	}
 
-    @GetMapping("/tarefa/{id}")
-    public Tarefa umaTarefa(@PathVariable Integer id) {
-        return tarefaRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Tarefa não encontrada com id: " + id));
-    }
+	@GetMapping("/{id}")
+	public Tarefa umaTarefa(@PathVariable Integer id) {
+		return tarefaRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Tarefa não encontrada com id: " + id));
+	}
 
-    @PostMapping("/tarefa")
-    public Tarefa criarTarefa(@RequestBody Tarefa tarefa) {
-        return tarefaRepository.save(tarefa);
-    }
+	@PostMapping
+	public Tarefa criarTarefa(@RequestBody Tarefa tarefa) {
+		return tarefaRepository.save(tarefa);
+	}
 
-    @DeleteMapping("/tarefa/{id}")
-    public void deletarTarefa(@PathVariable Integer id) {
-        tarefaRepository.deleteById(id);
-    }
+	@DeleteMapping("/{id}")
+	public void deletarTarefa(@PathVariable Integer id) {
+		tarefaRepository.deleteById(id);
+	}
 
-    @PutMapping("/tarefa/{id}")
-    public Tarefa atualizarTarefa(@PathVariable Integer id, @RequestBody Tarefa tarefaAtual) {
-        return tarefaRepository.findById(id)
-                .map(tarefa -> {
-                    tarefa.setCategoria(tarefaAtual.getCategoria());
-                    tarefa.setDescricao(tarefaAtual.getDescricao());
-                    tarefa.setStatus(tarefaAtual.getStatus());
-                    tarefa.setDataEntrega(tarefaAtual.getDataEntrega());
-                    tarefa.setVisivel(tarefaAtual.isVisivel());
-                    return tarefaRepository.save(tarefa);
-                })
-                .orElseThrow(() -> new RuntimeException("Tarefa não encontrada com id: " + id));
+	@PutMapping("/{id}")
+	public Tarefa atualizarTarefa(@PathVariable Integer id, @RequestBody Tarefa tarefaAtual) {
+		return tarefaRepository.findById(id).map(tarefa -> {
+			tarefa.setCategoria(tarefaAtual.getCategoria());
+			tarefa.setDescricao(tarefaAtual.getDescricao());
+			tarefa.setStatus(tarefaAtual.getStatus());
+			tarefa.setDataEntrega(tarefaAtual.getDataEntrega());
+			tarefa.setVisivel(tarefaAtual.isVisivel());
+			return tarefaRepository.save(tarefa);
+		}).orElseThrow(() -> new RuntimeException("Tarefa não encontrada com id: " + id));
 
-    }
+	}
 
 }
