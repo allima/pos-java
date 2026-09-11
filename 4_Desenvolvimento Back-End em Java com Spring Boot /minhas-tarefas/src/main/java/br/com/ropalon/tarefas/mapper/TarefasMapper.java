305c9dcb717a74722012dea520efdefc9f08bbcb
Alexandre de Lima <allima1991@gmail.com>
2026-09-11 11:40:40 -0300

    feat: update TarefasMapper to ignore 'status' field when mapping to Tarefa

--- 4_Desenvolvimento Back-End em Java com Spring Boot /minhas-tarefas/src/main/java/br/com/ropalon/tarefas/mapper/TarefasMapper.java
+++ 4_Desenvolvimento Back-End em Java com Spring Boot /minhas-tarefas/src/main/java/br/com/ropalon/tarefas/mapper/TarefasMapper.java
@@ -22,6 +22,7 @@ public interface TarefasMapper {
 	@Mapping(source = "categoriaId", target = "categoria.id")
 	@Mapping(source = "usuarioId", target = "usuario.id")
 	@Mapping(target = "visivel", constant = "false")
+	 @Mapping(target = "status", ignore = true)
 	Tarefa toTarefa(TarefaRequest tarefaRequest);
 
 }
